65e6c56d2e58ce964b1f460a9a68294ca0d45121
ngoncphucprodev <ngocphuc1399@gmail.com>
2023-04-08 13:16:43 +0700

    update statsu

--- server/src/route/routeUser.js
+++ server/src/route/routeUser.js
@@ -4,6 +4,9 @@ const UserController = require('../controller/usersController')
 
 router.post('/login', UserController.login)
 router.get('/all', UserController.show)
+router.get('/', (res) => {
+    res.json({ status: "success" })
+})
 router.post('/store', UserController.store)
 router.put('/edit/:id', UserController.edit)
 router.delete('/delete/:id', UserController.delete)
